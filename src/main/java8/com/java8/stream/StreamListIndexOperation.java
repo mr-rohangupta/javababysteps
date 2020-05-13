51f45cf7d77c316e6f0da4f642df472172633986
Rohan <rggupta.nagpur@gmail.com>
2020-05-13 20:42:17 +0530

    Added gitignore file and modified an example

--- src/main/java8/com/java8/stream/StreamListIndexOperation.java
+++ src/main/java8/com/java8/stream/StreamListIndexOperation.java
@@ -4,21 +4,25 @@ import com.java8.utils.Employee;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class StreamListIndexOperation {
     public static void main(String[] args) {
         StreamListIndexOperation streamListIndexOperation = new StreamListIndexOperation();
         List<com.java8.utils.Employee> employeeList = new ArrayList<>();
-        employeeList.add(new com.java8.utils.Employee("Male", "Rohan",null));
-        employeeList.add(new com.java8.utils.Employee("Female", "Disha",null));
-        employeeList.add(new com.java8.utils.Employee("Male", "Nilesh",null));
-        employeeList.add(new com.java8.utils.Employee("Male", "Suresh",null));
-        System.out.println("The Nth Employee is " + streamListIndexOperation.getNthEmployee(employeeList, 2));
+        //employeeList.add(new com.java8.utils.Employee("Male", "Rohan",null));
+        //employeeList.add(new com.java8.utils.Employee("Female", "Disha",null));
+        //employeeList.add(new com.java8.utils.Employee("Male", "Nilesh",null));
+        //employeeList.add(new com.java8.utils.Employee("Male", "Suresh",null));
+        Optional<Employee> nthEmployee = streamListIndexOperation.getNthEmployee(employeeList, 2);
+        System.out.println("The Nth Employee is::"+nthEmployee.get());
     }
 
-    public com.java8.utils.Employee getNthEmployee(List<Employee> employeeList, int index) {
-        return employeeList.stream().filter(employee -> employee.getGender().equalsIgnoreCase("Male"))
-                .collect(Collectors.toList()).get(index);
+    public Optional<Employee> getNthEmployee(List<Employee> employeeList, int index) {
+        Optional<Employee> employee = Optional.ofNullable(employeeList.stream()
+                .filter(e->e.getGender().equalsIgnoreCase("Male"))
+                .collect(Collectors.toList()).get(index));
+        return employee.isPresent() ? employee : Optional.empty();
     }
 }
