a619493dab23e3a41398e83ef6c997833deec99c
mr-rohangupta <rggupta.nagpur@gmail.com>
2020-12-28 11:56:25 +0530

    Missed commit

---
+++ src/main/java/com/java9/underscoreexample/UnderScoreDemo.java
@@ -0,0 +1,9 @@
+package com.java9.underscoreexample;
+
+public class UnderScoreDemo {
+    public static void main(String[] args) {
+        //TODO intentionally commented as _ is a reserved keywork in java 9
+//        int _ = 10;
+//        System.out.println(_);
+    }
+}
