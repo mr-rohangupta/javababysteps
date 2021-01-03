7d9f0381bc699b35bd024d5cc9cd8a5a2bdbf73c
mr-rohangupta <rggupta.nagpur@gmail.com>
2021-01-03 09:54:07 +0530

    Temp commit

--- src/main/java/com/java10/timebasedreleaseversioning/TimeBasedReleaseVersioningDemo.java
+++ src/main/java/com/java10/timebasedreleaseversioning/TimeBasedReleaseVersioningDemo.java
@@ -3,10 +3,10 @@ package com.java10.timebasedreleaseversioning;
 public class TimeBasedReleaseVersioningDemo {
 
     public static void main(String[] args) {
-        Runtime.Version version = Runtime.version();
-        System.out.println(version.feature());
-        System.out.println(version.interim());
-        System.out.println(version.update());
-        System.out.println(version.patch());
+         Runtime.Version version = Runtime.version();
+         System.out.println(version.feature());
+         System.out.println(version.interim());
+         System.out.println(version.update());
+         System.out.println(version.patch());
     }
 }
